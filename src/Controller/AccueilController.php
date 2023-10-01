5c153e5dc65f4ebf695095e6d7a5de5d1ea9aa73
add <add@LAPTOP-4HA986KK>
2023-10-01 19:21:01 +0200

    Phase 3 : utilisation de Twig

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,13 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @author add
  */
-class AccueilController {
+class AccueilController extends AbstractController {
     /**
      * 
      * @Route("/", name="accueil")
      * @return Response
      */
     public function index(): Response {
-         return new Response('Hello word!');
+         return $this->render("pages/accueil.html.twig");
     }
 }
