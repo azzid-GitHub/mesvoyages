45ba6e4786e926716519a9b6802e591cfa656c29
add <add@LAPTOP-4HA986KK>
2023-10-04 01:16:32 +0200

    Phase 9 : ORM (tri des enregistrements)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -3,10 +3,11 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\VisiteRepository;
 
 /**
  * Description of VoyagesController
@@ -49,6 +50,20 @@ class VoyagesController extends AbstractController {
      */
     public function sort($champs, $order): Response{
         $visites = $this->repository->findAllBy($champs, $order);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+
+    /**
+     * @Route("/voyages/recherche/{champs}", name="voyagesFilter")
+     * @param type $champs
+     * @param Request $request
+     * @return Response
+     */
+    public function findAllEqual($champs, Request $request): Response {
+        $valeur = $request->get("Recherche");
+        $visites = $this->repository->findByEqualValue($champs, $valeur);
         return $this->render("pages/voyages.html.twig", [
             'visites' => $visites
         ]);
